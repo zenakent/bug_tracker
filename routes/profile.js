edb3c8f71191dde1ce944e8b3a7c540d35c8f90d
Maui Torio <mauiandsonsph@yahoo.com>
2020-02-11 22:13:01 +0800

    added profile page, users can update their profiles

---
+++ routes/profile.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models')
+
+const {
+  isLoggedIn,
+} = require("../middleware/index.js");
+
+router.get('/', isLoggedIn, async function (req, res) {
+  try {
+    let foundUser = await db.User.findById(req.user._id)
+    res.render('profile/index', {
+      foundUser
+    })
+  } catch (error) {
+    console.log(error)
+    res.redirect('back')
+  }
+})
+
+router.put('/update/:id', async function (req, res) {
+  try {
+    let foundUser = await db.User.findByIdAndUpdate(req.user._id)
+    foundUser.first_name = req.body.first_name
+    foundUser.last_name = req.body.last_name
+    foundUser.email = req.body.email
+    foundUser.save()
+    res.redirect('/profile')
+  } catch (error) {
+    console.log(error)
+    res.redirect('back')
+  }
+})
+
+module.exports = router;
