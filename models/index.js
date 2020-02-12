62ff696ca4834261a0d9390bd4b4e1243b3b8887
Maui Torio <mauiandsonsph@yahoo.com>
2020-02-12 09:39:55 +0800

    added dotenv and env file and connected to an onlineDB

--- models/index.js
+++ models/index.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://localhost:27017/bug_tracker", {
+// mongoose.connect("mongodb://localhost:27017/bug_tracker", {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true,
+//   // useCreateIndex: true,
+// })
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASS}@cluster0-s8v4h.mongodb.net/test?retryWrites=true&w=majority`, {
+  dbName: "bug_tracker",
   useUnifiedTopology: true,
   useNewUrlParser: true,
-  // useCreateIndex: true,
+  useCreateIndex: true,
 })
 
 mongoose.Promise = Promise
