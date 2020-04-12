932f92495afcb0c3cc87ce571287ecc788e639be
Maui Torio <mauiandsonsph@yahoo.com>
2020-04-13 07:32:30 +0800

    changed mongodb to local

--- models/index.js
+++ models/index.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect("mongodb://localhost:27017/bug_tracker", {
-//   useUnifiedTopology: true,
-//   useNewUrlParser: true,
-//   // useCreateIndex: true,
-// })
-
-mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASS}@cluster0-s8v4h.mongodb.net/test?retryWrites=true&w=majority`, {
-  dbName: 'bug_tracker',
+mongoose.connect("mongodb://localhost:27017/bug_tracker", {
   useUnifiedTopology: true,
   useNewUrlParser: true,
-  useCreateIndex: true
-});
+  useCreateIndex: true,
+})
+
+// mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASS}@cluster0-s8v4h.mongodb.net/test?retryWrites=true&w=majority`, {
+//   dbName: 'bug_tracker',
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true,
+//   useCreateIndex: true
+// });
 
 mongoose.Promise = Promise;
 
